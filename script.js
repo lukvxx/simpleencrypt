04911811349c0ff6660655ca559b3c5be2578582
Lukas Deward <lukas@deward.de>
2022-12-13 21:10:55 +0100

    fixed performance issue

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict"
 
 function ec(value, key) {
-    key = key.repeat(Math.ceil(value.length/key.length))
-    return value.split("").map((char, i) => String.fromCodePoint(char.charCodeAt() + key.split("")[i].charCodeAt())).join("")
+    key = key.repeat(Math.ceil(value.length/key.length)).split("")
+    return value.split("").map((char, i) => String.fromCodePoint(char.charCodeAt() + key[i].charCodeAt())).join("")
 }
 
 
 function dc(value, key) {
-    key = key.repeat(Math.ceil(value.length/key.length))
-    return value.split("").map((char, i) => String.fromCodePoint(char.charCodeAt() - key.split("")[i].charCodeAt())).join("")
+    key = key.repeat(Math.ceil(value.length/key.length)).split("")
+    return value.split("").map((char, i) => String.fromCodePoint(char.charCodeAt() - key[i].charCodeAt())).join("")
 }
